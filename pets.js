893ce1068e04459d5a62d9233fc8a650c5487141
wallytreats <nicklawrenceemail@gmail.com>
2017-05-24 16:43:10 -0600

    add Node filesystem

--- pets.js
+++ pets.js
@@ -4,11 +4,47 @@ var pets = JSON.parse(fs.readFileSync('pets.json', 'utf8'));
 // console.log(process.argv[2]);
 // console.log(process.argv[3]);
 var action = process.argv[2];
-var index = process.argv[3];
-// if (action !== 'read' || 'create' || 'update' || 'destroy') {
-//   console.error(new Error('Usage: node pets.js [read | create | update | destroy]'));
-// }
+var arg = process.argv[3];
 
-if(action === 'read'){
-  console.log(pets[index]);
+if (!action) {
+  console.error('Usage: node pets.js [read | create | update | destroy]');
+  process.exit(1);
+}
+
+else if(action === 'read'){
+  if (arg){
+    console.log(pets[arg]);
+  }
+  else {
+    console.log(pets);
+  }
+}
+if (action === 'create'){
+  if(process.argv.length === 6){
+    var ageVal = parseInt(process.argv[3]);
+    var kindVal = process.argv[4];
+    var nameVal = process.argv[5];
+    var petObj = {
+      age: ageVal,
+      kind: kindVal,
+      name: nameVal
+    };
+
+
+    // console.log(current);
+    pets.push(petObj);
+    var newPet = JSON.stringify(pets);
+    fs.writeFile('pets.json', newPet, function (err) {
+      if(err){
+        console.error('awwwww');
+      }
+      else{
+        console.log(petObj);
+      }
+    });
+  }
+  else if (process.argv.length !== 6){
+    console.error('Usage: node pets.js create AGE KIND NAME');
+    process.exit(1);
+  }
 }
