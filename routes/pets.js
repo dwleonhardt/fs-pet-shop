45a201e20ada4199ff96778c7ae3b5bba8ebf781
dwleonhardt <dwleonhardt@gmail.com>
2017-05-25 15:18:45 -0600

    express server passes all tests

--- routes/pets.js
+++ routes/pets.js
@@ -4,11 +4,25 @@ const fs = require('fs');
 const path = require('path');
 
 
+
 router.get('/', function(req, res, next){
   fs.readFile('./pets.json', 'utf8', function(err, data){
     data = JSON.parse(data);
     res.send(data);
   });
+  next();
+});
+
+router.get('/:id', function(req, res, next){
+    fs.readFile('./pets.json', 'utf8', function(err, data){
+      data = JSON.parse(data);
+      if (req.params.id < data.length && req.params.id > -1) {
+        res.send(data[req.params.id]);
+      }
+      else {
+        res.sendStatus(404);
+      }
+    });
 });
 
 module.exports = router;
